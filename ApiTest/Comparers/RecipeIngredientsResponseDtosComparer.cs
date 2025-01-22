e0a129810df31f63cded196e89e284e77b1e0469
kieranla72 <kieran.lang@astraappivate.co.uk>
2025-01-22 08:54:20 +0000

    Removed all logs

--- ApiTest/Comparers/RecipeIngredientsResponseDtosComparer.cs
+++ ApiTest/Comparers/RecipeIngredientsResponseDtosComparer.cs
@@ -61,18 +61,6 @@ public class RecipeIngredientsResponseDtosComparer : IEqualityComparer<List<Reci
 
     private bool IngredientResponseDtosAreEqual(RecipeIngredientsResponseDto ingredient1, RecipeIngredientsResponseDto ingredient2)
     {
-        // Compare Id, Title
-        if (ingredient1.Id != ingredient2.Id) Console.WriteLine("ingredient ids are off");
-        if (ingredient1.Title != ingredient2.Title) Console.WriteLine("ingredient titles are off");
-        if (!DateTime.Equals(ingredient1.CreatedTimestamp, ingredient2.CreatedTimestamp))
-        {
-            Console.WriteLine(ingredient1.Title);
-            Console.WriteLine("ingredient CreatedTimestamp are off. 1st date: " +  $"{ingredient1.CreatedTimestamp:o}" + " 2nd date: " +
-                              $"{ingredient2.CreatedTimestamp:o}");
-        }
-        if (ingredient1.Comment != ingredient2.Comment) Console.WriteLine("ingredient Comment are off");
-        if (ingredient1.Quantity != ingredient2.Quantity) Console.WriteLine("ingredient Quantity are off");
-        if (ingredient1.UnitOfQuantity != ingredient2.UnitOfQuantity) Console.WriteLine("ingredient UnitOfQuantity are off");
         return ingredient1.Id == ingredient2.Id &&
                ingredient1.Title == ingredient2.Title &&
                ingredient1.CreatedTimestamp.ToString("yyyy-MM-dd HH:mm:ss") ==
